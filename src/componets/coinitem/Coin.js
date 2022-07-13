a452653899ac9dd9c2d3ea98e22559b5b7a03013
JitGan <jiteshgandluru@gmail.com>
2022-07-13 13:42:59 -0400

    Added Files

---
+++ src/componets/coinitem/Coin.js
@@ -0,0 +1,37 @@
+import React from "react"
+import "./Coin.css"; 
+
+const Coin = ({
+    name, 
+    price, 
+    symbol, 
+    marketcap, 
+    volume, 
+    image, 
+    priceChange,
+}) => {
+    return (
+        <div className="cryptoCoin">
+            <img src={image} alt={'${name}'} className="coinLogo" />
+            <div className="coinNameWrap">
+                <h1 className="coinName">{name}</h1>
+                <p className="coinSymbol">{symbol}</p>
+            </div>
+            <p className="coinPrice">${price.toLocaleString()}</p>
+            <p className="coinMarketcap">Market Cap: ${marketcap.toLocaleString()}</p>
+            <p className="coinValue">Volume (24h): ${volume.toLocaleString()} </p>
+            {priceChange < 0 ?  (
+                <div className="priceContainerDOWN">
+                    <i className="fas fa-angle-down "></i>
+                    <p className="priceChange">{priceChange.toFixed(2)}%</p>
+                </div>
+            ):(
+                <div className="priceContainerUP">
+                    <i className="fas fa-angle-up "></i>
+                    <p className="priceChange">{priceChange.toFixed(2)}%</p>
+                    </div>
+        )}
+     </div>
+    );
+};
+export default Coin;
